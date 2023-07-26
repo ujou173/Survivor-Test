2d426068aeb78a572de0d3005b28fc32ea5ce23b
Ro sumin <117888227+ujou173@users.noreply.github.com>
2023-07-26 21:18:27 +0900

    player 이동 구현

--- Assets/Undead Survivor/Scripts/Player.cs
+++ Assets/Undead Survivor/Scripts/Player.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  public Vector2 inputVec;
+  public float speed;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  Rigidbody2D rigid;
+
+  // Start is called before the first frame update
+  void Awake()
+  {
+    rigid = GetComponent<Rigidbody2D>();
+  }
+
+  void FixedUpdate()
+  {
+    // // 1. 힘을 준다
+    // rigid.AddForce(inputVec);
+
+    // // 2. 속도 제어
+    // rigid.velocity = inputVec;
+
+    // // 3. 위치 이동
+    // rigid.MovePosition(rigid.position + inputVec);
+
+    Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
+    rigid.MovePosition(rigid.position + nextVec);
+  }
+
+  void OnMove(InputValue value)
+  {
+    inputVec = value.Get<Vector2>();
+  }
 }

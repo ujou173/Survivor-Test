22127f051a1eb54b2bc70609be15d93fd74411d6
Ro sumin <117888227+ujou173@users.noreply.github.com>
2023-07-28 20:14:09 +0900

    맵 이동시 가끔 일부 타일이 이동하지 않거나 다른 위치로 이동하는 현상 발생 / 해결을 위해 다른 무한맵 생성 방식을 찾아보거나 무한맵을 포기해야 할 것으로 생각 됨

--- Assets/Undead Survivor/Scripts/Reposition.cs
+++ Assets/Undead Survivor/Scripts/Reposition.cs
@@ -12,14 +12,23 @@ public class Reposition : MonoBehaviour
     Vector3 PlayerPos = GameManager.instance.player.transform.position;
     Vector3 myPos = transform.position;
 
-    // Mathf.Abs() => 괄호 안의 값을 절대값으로 만드는 함수
-    float diffX = Mathf.Abs(PlayerPos.x - myPos.x);
-    float diffY = Mathf.Abs(PlayerPos.y - myPos.y);
+    float dirX = PlayerPos.x - myPos.x;
+    float dirY = PlayerPos.y - myPos.y;
 
-    // inputSystem 사용시 캐릭터 방향을 알기 위한 로직
-    Vector3 playerDir = GameManager.instance.player.inputVec;
-    float dirX = playerDir.x < 0 ? -1 : 1;
-    float dirY = playerDir.y < 0 ? -1 : 1;
+    float diffX = Mathf.Abs(dirX);
+    float diffY = Mathf.Abs(dirY);
+
+    dirX = dirX > 0 ? 1 : -1;
+    dirY = dirY > 0 ? 1 : -1;
+
+    // // Mathf.Abs() => 괄호 안의 값을 절대값으로 만드는 함수
+    // float diffX = Mathf.Abs(PlayerPos.x - myPos.x);
+    // float diffY = Mathf.Abs(PlayerPos.y - myPos.y);
+
+    // // inputSystem 사용시 캐릭터 방향을 알기 위한 로직
+    // Vector3 playerDir = GameManager.instance.player.inputVec;
+    // float dirX = playerDir.x < 0 ? -1 : 1;
+    // float dirY = playerDir.y < 0 ? -1 : 1;
 
     switch (transform.tag)
     {
@@ -27,7 +36,7 @@ public class Reposition : MonoBehaviour
         // player가 X축 이동을 할 때
         if (diffX > diffY)
         {
-          transform.Translate(Vector3.right * dirX * 40);
+          transform.Translate(Vector3.right * dirX * 60);
         }
         // player가 Y축 이동을 할 때
         else if (diffX < diffY)

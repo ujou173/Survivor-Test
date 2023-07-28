4c93bf5bd8cc3534dfdb70c0925c1493d665afc4
Ro sumin <117888227+ujou173@users.noreply.github.com>
2023-07-28 17:34:30 +0900

    캐릭터가 진행방향 바라보게 만들기 / 셀 애니메이션 적용

--- Assets/Undead Survivor/Scripts/Player.cs
+++ Assets/Undead Survivor/Scripts/Player.cs
@@ -9,11 +9,15 @@ public class Player : MonoBehaviour
   public float speed;
 
   Rigidbody2D rigid;
+  SpriteRenderer spriter;
+  Animator anim;
 
   // Start is called before the first frame update
   void Awake()
   {
     rigid = GetComponent<Rigidbody2D>();
+    spriter = GetComponent<SpriteRenderer>();
+    anim = GetComponent<Animator>();
   }
 
   void FixedUpdate()
@@ -34,5 +38,18 @@ public class Player : MonoBehaviour
   void OnMove(InputValue value)
   {
     inputVec = value.Get<Vector2>();
+  }
+
+  void LateUpdate()
+  {
+    anim.SetFloat("Speed", inputVec.magnitude);
+
+    if (inputVec.x != 0)
+    {
+      spriter.flipX = inputVec.x < 0;
+      // flipX 속성은 불리언 값을 입력 받는다. 나는 왼쪽 방향키가 눌렸을 때, 즉 뒤로 갈 때, 다시말해 inputVec.x가 -1 일때 flipX 를 사용할 것이므로
+      // true / false 대신에 조건식을 넣어 불리언 값을 반환하도록 하여 코드를 줄일 수 있다.
+      // 왼쪽 방향키가 눌리면 inputVec.x는 -1이므로 식을 만족하여 true가 반환 되고, 오른쪽 방향키가 눌리는 동안에는 식이 만족하지 못하여 false가 반환된다.
+    }
   }
 }

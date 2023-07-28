4cef1d827c15d5089aab0ae44b8dd92b62df3057
Ro sumin <117888227+ujou173@users.noreply.github.com>
2023-07-28 19:06:15 +0900

    Cinemachine 패키지 설치 (카메라 관련 패키지) / 플레이어 위치에 따라 자동으로 타일맵이 이동하도록하여 무한맵을 구현

---
+++ Assets/Undead Survivor/Scripts/Reposition.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reposition : MonoBehaviour
+{
+  void OnTriggerExit2D(Collider2D collision)
+  {
+    if (!collision.CompareTag("Area"))
+      return;
+
+    Vector3 PlayerPos = GameManager.instance.player.transform.position;
+    Vector3 myPos = transform.position;
+
+    // Mathf.Abs() => 괄호 안의 값을 절대값으로 만드는 함수
+    float diffX = Mathf.Abs(PlayerPos.x - myPos.x);
+    float diffY = Mathf.Abs(PlayerPos.y - myPos.y);
+
+    // inputSystem 사용시 캐릭터 방향을 알기 위한 로직
+    Vector3 playerDir = GameManager.instance.player.inputVec;
+    float dirX = playerDir.x < 0 ? -1 : 1;
+    float dirY = playerDir.y < 0 ? -1 : 1;
+
+    switch (transform.tag)
+    {
+      case "Ground":
+        // player가 X축 이동을 할 때
+        if (diffX > diffY)
+        {
+          transform.Translate(Vector3.right * dirX * 40);
+        }
+        // player가 Y축 이동을 할 때
+        else if (diffX < diffY)
+        {
+          transform.Translate(Vector3.up * dirY * 40);
+        }
+        break;
+      case "Enemy":
+
+        break;
+    }
+  }
+}

38de2e201b382e097355231314c37570da2f8274
Ro sumin <117888227+ujou173@users.noreply.github.com>
2023-08-02 14:55:55 +0900

    Enemy 스프라이트 생성 / 플레이어를 추적하는 로직 작성

---
+++ Assets/Undead Survivor/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+  // 플레이어를 추적할 속도
+  public float speed;
+
+  // 추적할 대상(플레이어), 물리를 사용해서 추적할 것이기 때문에 Rigidbody2D 사용
+  public Rigidbody2D target;
+
+  // 몬스터가 살아있는지 죽어있는지 판별하기 위한 bool 변수
+  bool isLive;
+
+  Rigidbody2D rigid;
+  SpriteRenderer spriter;
+
+
+  void Awake()
+  {
+    rigid = GetComponent<Rigidbody2D>();
+    spriter = GetComponent<SpriteRenderer>();
+  }
+
+  // Update is called once per frame
+  void FixedUpdate()
+  {
+    // Enemy가 움직일 방향 => 플레이어의 위치와 Enemy의 위치의 차이를 이용해 방향을 구함
+    Vector2 dirVec = target.position - rigid.position;
+    Vector2 nextVec = dirVec.normalized * speed * Time.fixedDeltaTime;
+    rigid.MovePosition(rigid.position + nextVec);
+    // 충돌에 의한 물리 속도가 이동에 영향을 주지 않도록 값을 0으로 고정
+    rigid.velocity = Vector2.zero;
+  }
+}

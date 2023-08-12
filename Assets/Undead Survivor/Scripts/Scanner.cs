6ce22c1f53d04ad3773010e300fe72c460df2792
Ro sumin <117888227+ujou173@users.noreply.github.com>
2023-08-13 01:12:17 +0900

    가장 가까운 적을 선택하는 코드 작성

--- Assets/Undead Survivor/Scripts/Scanner.cs
+++ Assets/Undead Survivor/Scripts/Scanner.cs
@@ -20,5 +20,27 @@ public class Scanner : MonoBehaviour
     {
         // CircleCastAll => 원형 범위로 검색 // CircleCasetAll(캐스팅 시작 위치, 원의 반지름, 캐스팅 방향, 캐스팅 길이, 대상 레이어)
         targets = Physics2D.CircleCastAll(transform.position, sacnRange, Vector2.zero, 0, targetLayer);
+        nearestTarget = GetNearest();
+    }
+
+    Transform GetNearest()
+    {
+        Transform result = null;
+        float diff = 100;
+
+        foreach (RaycastHit2D target in targets)
+        {
+            Vector3 myPos = transform.position;
+            Vector3 targetPos = target.transform.position;
+            float curDiff = Vector3.Distance(myPos, targetPos);
+
+            if (curDiff < diff)
+            {
+                diff = curDiff;
+                result = target.transform;
+            }
+        }
+
+        return result;
     }
 }

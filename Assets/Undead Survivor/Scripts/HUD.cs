7b9d418c2e9f798a3acd48653565f1c72b9c2337
Ro sumin <117888227+ujou173@users.noreply.github.com>
2023-08-25 08:31:49 +0900

    HUD 제작을 위한 기반 스크립트 작성

---
+++ Assets/Undead Survivor/Scripts/HUD.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// Text를 사용하기 위해 추가되는 클래스
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    // enum -> 열겨형
+    public enum InfoType { Exp, Level, Kill, Time, Health }
+    public InfoType type;
+
+    Text myText;
+    Slider mySlider;
+
+    void Awake()
+    {
+        myText = GetComponent<Text>();
+        mySlider = GetComponent<Slider>();
+    }
+
+    // 연산이 다 되고 끝나고나서 실행 LateUpdate()
+    void LateUpdate()
+    {
+        switch (type)
+        {
+            case InfoType.Exp:
+
+                break;
+            case InfoType.Level:
+
+                break;
+            case InfoType.Kill:
+
+                break;
+            case InfoType.Time:
+
+                break;
+            case InfoType.Health:
+
+                break;
+        }
+    }
+}

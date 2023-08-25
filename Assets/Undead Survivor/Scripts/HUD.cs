1ec83039bfe038fddbf62df0eb4c268521c014ca
Ro sumin <117888227+ujou173@users.noreply.github.com>
2023-08-25 09:10:58 +0900

    레벨과 kill count HUD 제작 및 적용

--- Assets/Undead Survivor/Scripts/HUD.cs
+++ Assets/Undead Survivor/Scripts/HUD.cs
@@ -30,10 +30,12 @@ public class HUD : MonoBehaviour
                 mySlider.value = curExp / maxExp;
                 break;
             case InfoType.Level:
-
+                // text는 문자열값만 들어갈 수 있다.
+                // Format("포맷의 타입", 포맷을 적용 할 데이터) / "Lv.{0:F0}" => Lv. 이라는 텍스트와 {데이터의 index 순번(GameManager.instance.level): 데이터 포맷(F0 -> 소수점 0번째 자리까지)}
+                myText.text = string.Format("Lv.{0:F0}", GameManager.instance.level);
                 break;
             case InfoType.Kill:
-
+                myText.text = string.Format("{0:F0}", GameManager.instance.kill);
                 break;
             case InfoType.Time:
 

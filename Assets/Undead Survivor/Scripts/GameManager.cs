5f14544a33780c401819639382799d732112bffc
Ro sumin <117888227+ujou173@users.noreply.github.com>
2023-08-25 09:25:58 +0900

    플레이어 체력바 구현

--- Assets/Undead Survivor/Scripts/GameManager.cs
+++ Assets/Undead Survivor/Scripts/GameManager.cs
@@ -11,6 +11,8 @@ public class GameManager : MonoBehaviour
   public float maxGameTime = 2 * 10f;
 
   [Header("# Player Info")]
+  public int health;
+  public int maxHealth = 100;
   public int level;
   public int kill;
   public int exp;
